df8e53594f918e4246c0e04f5185aff6400f13df
Fvnkyy Botz <118755213+rellzxd@users.noreply.github.com>
2023-01-28 00:00:32 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -6,15 +6,15 @@ let mdjon = args.join(' ')
 res = await mediafireDl(mdjon)
 result = `「 𝗠𝗘𝗗𝗜𝗔𝗙𝗜𝗥𝗘 」
 *Data Berhasil Didapatkan!*
-🆔 Nᴀᴍᴇ : ${res[0].nama}
-📊 Sɪᴢᴇ : ${res[0].size}
-💬 Lɪɴᴋ : ${res[0].link}
+🆔 Name : ${res[0].nama}
+📊 Size : ${res[0].size}
+💬 Link : ${res[0].link}
 _Tunggu kak, sedang proses kirim file..._`
 m.reply(result)
 //await sleep(100)
 conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
 }
-handler.help = ['mediafire']
+handler.help = ['mediafire <url>']
 handler.tags = ['downloader']
 handler.command = /^(mediafire)$/i
 
