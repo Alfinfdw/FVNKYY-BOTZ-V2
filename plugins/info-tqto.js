ab7582fb5446ca2f2b1760f257794f18023c8a69
Fvnkyy Botz <118755213+rellzxd@users.noreply.github.com>
2023-01-28 16:14:47 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -4,4 +4,85 @@
 * jangan dihilangin, boleh di tambahin 🐦
 **/
 
-const _0xbaf57d=_0x36c1;(function(_0x4040fc,_0x3eb5e6){const _0x2bc561=_0x36c1,_0x622e74=_0x4040fc();while(!![]){try{const _0x174e65=parseInt(_0x2bc561(0xcd))/0x1+parseInt(_0x2bc561(0xca))/0x2+parseInt(_0x2bc561(0xc7))/0x3+-parseInt(_0x2bc561(0xcb))/0x4+parseInt(_0x2bc561(0xc8))/0x5*(parseInt(_0x2bc561(0xd1))/0x6)+parseInt(_0x2bc561(0xd4))/0x7*(-parseInt(_0x2bc561(0xc9))/0x8)+-parseInt(_0x2bc561(0xd0))/0x9;if(_0x174e65===_0x3eb5e6)break;else _0x622e74['push'](_0x622e74['shift']());}catch(_0x3f99b6){_0x622e74['push'](_0x622e74['shift']());}}}(_0x2450,0x2bdc3));let handler=async(_0x272391,{conn:_0x33193f})=>{const _0x19e2c3=_0x36c1;bear=_0x19e2c3(0xd3);let _0x129c0f=_0x19e2c3(0xc3);_0x33193f[_0x19e2c3(0xcf)](_0x272391['chat'],fla+_0x19e2c3(0xc5),_0x129c0f,wm2,_0x19e2c3(0xc6),_0x19e2c3(0xcc),_0x272391);};function _0x2450(){const _0x4b279d=['tags','command','Big\x20Thanks\x20to\x0a\x0a>\x20Allah\x20swt\x0a>\x20My\x20Team\x0a>\x20Nurutomo\x0a>\x20Penyedia\x20RestApi\x0a>\x20AlyaXd\x0a>\x20Verdihatorou\x0a>\x20Radhin\x0a>\x20TioXd\x0a>\x20Xteam\x0a>\x20Lolhuman\x0a>\x20All\x20Owner','team','Thanks\x20To','Makasih','1015089oPaCkd','711745oICuNw','8GfoDiG','529964vshpEI','755164SknEGE','thanks','211719mFnKAf','info','sendButtonImg','4995594XJIxkn','6MbhgsK','exports','Source\x20Code','237335znzobx'];_0x2450=function(){return _0x4b279d;};return _0x2450();}function _0x36c1(_0x15346e,_0x4bafa4){const _0x24504b=_0x2450();return _0x36c1=function(_0x36c140,_0x19f6a3){_0x36c140=_0x36c140-0xc3;let _0x41c39e=_0x24504b[_0x36c140];return _0x41c39e;},_0x36c1(_0x15346e,_0x4bafa4);}handler['help']=['tqto',_0xbaf57d(0xc4)],handler[_0xbaf57d(0xd5)]=[_0xbaf57d(0xce)],handler[_0xbaf57d(0xd6)]=/^(tqto|team)$/i,module[_0xbaf57d(0xd2)]=handler;
+import fetch from 'node-fetch'
+
+let handler = async (m) => {
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    //let wibu = `https://api.zacros.my.id/randomimg/loli`
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
+    let txt = await res.text()
+    let arr = txt.split('\n')
+    let zeen = arr[Math.floor(Math.random() * arr.length)]
+    let thumb = await(await fetch(zeen)).buffer()
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
+*❉ Adiwajshing:*
+https://github.com/adiwajshing
+*❉ Nurutomo:*
+https://github.com/Nurutomo
+*❉ Istikmal:* 
+https://github.com/BochilGaming
+*❉ Ariffb:*
+https://github.com/Ariffb25
+*❉ Ilman:*
+https://github.com/ilmanhdyt
+*❉ Amirul:*
+https://github.com/amiruldev20
+*❉ Rasel:*
+https://github.com/raselcomel
+*❉ Fatur:*
+https://github.com/Ftwrr
+*❉ Rominaru:*
+https://github.com/Rominaru
+*❉ Kannachann:*
+https://github.com/Kannachann
+*❉ The.sad.boy01:*
+https://github.com/kangsad01
+*❉ Ameliascrf:*
+https://github.com/Ameliascrf
+*❉ Fokus ID:*
+https://github.com/Fokusdotid
+*❉ Johannes:*
+https://github.com/Johannes2803
+*❉ BrunoSobrino:*
+https://github.com/BrunoSobrino
+*❉ Krisna:*
+https://github.com/NevtBotz
+*❉ LitRHap:*
+https://github.com/LitRHap
+*❉ Rlxfly:*
+https://github.com/Rlxfly
+*❉ Aine:*
+https://github.com/Aiinne
+*❉ Papah-Chan:*
+https://github.com/FahriAdison
+*▸ - - - —「 Penulis Ulang 」— - - - ◂*
+*❉ ImYanXiao:*
+https://github.com/ImYanXiao
+*▸ - - - —「 Thanks For Suhu 」— - - - ◂*
+*❉ Kannachan:*
+*❉ The.sadboy.01:*
+*❉ Papah-Chan:*
+*❉ Johannes:*
+*❉ Krisna:*
+*❉ Rlxfly:*
+
+`
+conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaType: "VIDEO",
+    description: "https://Instagram.com/Xiao_yan_21", 
+    title: 'Elaina-MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+}
+} })
+     }
+handler.help = ['tqto']
+handler.tags = ['main','info']
+handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
+export def handler
