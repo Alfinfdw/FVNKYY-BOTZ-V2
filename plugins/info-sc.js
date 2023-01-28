2a4f86f6b9a9a53fb93837191ff72fb83f6eb9b3
Fvnkyy Botz <118755213+rellzxd@users.noreply.github.com>
2023-01-28 16:52:03 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Ngak Ada!😜
+🎐Sc Fvnkyy-V2 Only Private Kak!
 `
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
